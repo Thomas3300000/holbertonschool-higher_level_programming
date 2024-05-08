82de940e710dbdb655f934070cc047fb7f689990
Thomas3300000 <8844@holbertonstudents.com>
2024-05-08 22:58:30 +0200

    Fix print in the wrong place  print uses Print uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,6 @@ def uppercase(str):
     for cpt in (str):
         if ord(cpt) >= 97 and ord(cpt) <= 122:
             cpt = chr(ord(cpt) - 32)
-            
-    print("{}".format(cpt), end="")
-    print()
+
+        print("{}".format(cpt), end="")
+    print("")
