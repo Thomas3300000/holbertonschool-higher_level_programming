ebe598e22345a5f97298033fc0703eb0961a8770
Thomas3300000 <8844@holbertonstudents.com>
2024-05-08 12:05:16 +0200

    Fix stdout negative last digit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = abs(number) % 10
+
+if number > 0:
+    last = number % 10
+else:
+    last = number % -10
 
 print("Last digit of {} is {} and is".format(number, last), end=" ")
 
@@ -11,4 +15,3 @@ elif last == 0:
     print("0")
 else:
     print("less than 6 and not 0")
-    
