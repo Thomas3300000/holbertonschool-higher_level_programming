c858d769c3eae64b30dce1559af6b7ab646b532b
Thomas3300000 <8844@holbertonstudents.com>
2024-05-14 11:56:51 +0200

    Fix if

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for cpt in my_list:
-        print("{}".format(cpt))
+    if isinstance(my_list, list):
+        my_list.reverse()
+        for cpt in my_list:
+            print("{:d}".format(cpt))
