46fa12a68a0a4852b1393ac268d34d0fd52df867
Thomas3300000 <8844@holbertonstudents.com>
2024-05-14 12:10:02 +0200

    Fix pycodestyle

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for cpt in range(len(matrix)):
         for cpt2 in range(len(matrix[cpt])):
             if cpt2 != 0:
-                print (" ", end="")
+                print(" ", end="")
             print("{:d}".format(matrix[cpt][cpt2]), end=" ")
         print()
