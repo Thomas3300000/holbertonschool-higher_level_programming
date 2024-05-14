33e3de6cd2b22efb4d64f2036d3a8a5e7e6ccefe
Thomas3300000 <8844@holbertonstudents.com>
2024-05-14 12:15:40 +0200

    Fix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for cpt in range(len(matrix)):
         for cpt2 in range(len(matrix[cpt])):
-            if cpt2 != 0:
+             print("{:d}".format(matrix[cpt][cpt2]), end=" ")
+             if cpt2 != (len(matrix[cpt]) - 1):
                 print(" ", end="")
-            print("{:d}".format(matrix[cpt][cpt2]), end=" ")
         print()
