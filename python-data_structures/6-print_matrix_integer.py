3de9af30dfcdf6c5443dec2fc9e31c9117fb1188
Thomas3300000 <8844@holbertonstudents.com>
2024-05-14 12:04:24 +0200

    Fix Matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for cpt in matrix:
-        for cpt2 in cpt:
-            print("{}".format(cpt2), end=" ")
+    for cpt in range(len(matrix)):
+        for cpt2 in range(len(matrix[cpt])):
+            if cpt2 != 0:
+                print (" ", end="")
+            print("{:d}".format(matrix[cpt][cpt2], end=" ")
         print()
