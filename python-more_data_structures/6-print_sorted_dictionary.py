8564207720b0cdfd15f446149d0af7b7ed00d4ac
Thomas3300000 <8844@holbertonstudents.com>
2024-05-15 14:51:04 +0200

    Print in order keys

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    order = sorted(a_dictionary)
+    for cpt in order:
+        print("{}: {}".format(cpt,a_dictionary[cpt]))
+        
