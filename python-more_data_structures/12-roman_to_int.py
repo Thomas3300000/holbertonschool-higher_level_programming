aad2b0351b8a711993f3763610e5fc82de14af1f
Thomas3300000 <8844@holbertonstudents.com>
2024-05-16 00:16:07 +0200

    Fix cindition if a string or none

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return (0)
+
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
     previous = 0
 
     for letters in roman_string:
+        if letters not in roman:
+            return (0)
         result += roman[letters]
         if roman[letters] > previous:
             result -= 2 * previous
