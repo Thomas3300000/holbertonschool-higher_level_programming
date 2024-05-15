7ab51acefd187a2e15de842a1907f50ef1c3059d
Thomas3300000 <8844@holbertonstudents.com>
2024-05-15 11:40:13 +0200

    Fix error commit Square Matrix

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new = matrix.copy()
+    for cpt in range(len(matrix)):
+            new[cpt] = list(map(lambda x: x**2, matrix[cpt]))
+            
+    return (new)
