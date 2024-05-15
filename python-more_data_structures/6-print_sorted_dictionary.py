797ccfeaf8774ea0443a928b87c1c9a74492ec3a
Thomas3300000 <8844@holbertonstudents.com>
2024-05-15 14:52:10 +0200

    Fix space after , Print in order keys

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,5 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     order = sorted(a_dictionary)
     for cpt in order:
-        print("{}: {}".format(cpt,a_dictionary[cpt]))
-        
+        print("{}: {}".format(cpt, a_dictionary[cpt]))
