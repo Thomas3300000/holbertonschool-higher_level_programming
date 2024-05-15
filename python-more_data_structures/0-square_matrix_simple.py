71f46b4dcf43ba775b5c2982289910204fb465b5
Thomas3300000 <8844@holbertonstudents.com>
2024-05-15 11:47:00 +0200

    Fix pycodestyle  Square Matrix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,6 @@
 def square_matrix_simple(matrix=[]):
     new = matrix.copy()
     for cpt in range(len(matrix)):
-            new[cpt] = list(map(lambda x: x**2, matrix[cpt]))
-            
+        new[cpt] = list(map(lambda x: x**2, matrix[cpt]))
+
     return (new)
