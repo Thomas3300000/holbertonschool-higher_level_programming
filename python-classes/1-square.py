974757ad01cba4ce86fc433d67aa537382c55000
Thomas3300000 <8844@holbertonstudents.com>
2024-05-19 22:30:48 +0200

    Fix indentation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -4,9 +4,9 @@
 
 class Square:
     """Represents a square"""
-def __init__(self, size):
-    """Initialize a new square
-    Args:
+    def __init__(self, size):
+        """Initialize a new square
+        Args:
         size (int): The size of the new square
-    """
-    self.__size = size
+        """
+        self.__size = size
