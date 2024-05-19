b299f5a231f4cf252d77a3d09e9528f8afd825d8
Thomas3300000 <8844@holbertonstudents.com>
2024-05-19 22:50:52 +0200

    Add Instantiation

---
+++ python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """Represents a square"""
+    def __init__(self, size=0):
+        """Initialize a new square
+        
+        Args:
+        size (int): The size of the new square
+        
+        Raises:
+        TypeError: if size is not an integer
+        ValueError: if size is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
