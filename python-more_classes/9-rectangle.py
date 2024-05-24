7d47668384fc8ed1756f082b58ef9bc71836096b
Thomas3300000 <8844@holbertonstudents.com>
2024-05-24 19:52:50 +0200

    Fix pycodestyle

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -80,7 +80,7 @@ class Rectangle:
             rect_1 (Rectangle): The first rectangle
             rect_2 (Rectangle): The second rectangle
         Raises:
-            TypeError: If either of rect_1 or rect_2 is not a Rectangle    
+            TypeError: If either of rect_1 or rect_2 is not a Rectangle
         """
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
