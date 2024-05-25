fe26efde8e3d30045934430eb04d61affb5239c9
Thomas3300000 <8844@holbertonstudents.com>
2024-05-25 23:43:44 +0200

    Define methode str

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Create class Square that inherits from Rectangle"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Define class Square"""
+
+    def __init__(self, size):
+        """Initialize square
+
+        Args:
+            size (int): The size of the new square
+        """
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def area(self):
+        """Return the area of the square"""
+        return super().area()
+
+    def __str__(self):
+        """Return the print() and str() representation of a Square"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
