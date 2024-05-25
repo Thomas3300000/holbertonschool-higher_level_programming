bcd39124c1b89bceb1bc55b149353203be31b6d7
Thomas3300000 <8844@holbertonstudents.com>
2024-05-25 14:30:54 +0200

    find type or object

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Create a function that returns True if the object is an instance or
+if object is an instance of a class that inherited from the specified class,
+otherwise False."""
+
+
+def is_kind_of_class(obj, a_class):
+    """Check if an object is an instance or if object is an instance of
+    a class that inherited from the specified class
+
+    Args:
+        obj: object
+        a_class: class
+    Returns:
+        True if obj is an instance or if object is an instance of
+        a_class
+        False if obj is not an instance or if object is not an instance
+        of a_class
+    """
+    if isinstance(obj, a_class):
+        return True
+    return False
