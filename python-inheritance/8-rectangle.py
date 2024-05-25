59cd59cd86b15a75f9099fbaae2e501ff2e1ee32
Thomas3300000 <8844@holbertonstudents.com>
2024-05-25 19:17:52 +0200

    Fix Define class Rectangle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -6,11 +6,21 @@ class BaseGeometry:
     """Define class BaseGeometry"""
 
     def area(self):
-        """Define method area"""
+        """Define method area
+        Raises:
+            Exception: area() is not implemented
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Define method integer_validator"""
+        """Define method integer_validator
+        Args:
+            name (str): The name of the parameter
+            value (int): The value of the parameter
+        Raises:
+            TypeError: if value is not an integer
+            ValueError: if value is less than or equal to 0
+        """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
@@ -27,7 +37,7 @@ class Rectangle(BaseGeometry):
             width (int): The width of the new rectangle
             height (int): The height of the new rectangle
         """
-        self.__width = width
-        self.__height = height
         self.integer_validator("width", width)
         self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
