a5ca795eefc2881b90c3e5dcd90e477fd46dbc04
Thomas3300000 <8844@holbertonstudents.com>
2024-05-25 16:50:28 +0200

    Fix docstyle

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Create a function that returns True if the object is an instance of a class 
-that inherited (directly or indirectly) from the specified class"""
+"""Create a function that returns True if the object is an instance"""
 
 
 def inherits_from(obj, a_class):
