5658a5ad6800ac4a1f7dc72663d945ce16edad1a
Thomas3300000 <8844@holbertonstudents.com>
2024-05-26 21:13:17 +0200

    Class verbolist

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+from typing import Iterable
+
+
+class VerboseList(list):
+    """Class VerboseList"""
+    
+    def append(self, item):
+        """Append an item to the list"""
+        super().append(item)
+        print("Added [{}] to the list".format(item))
+        
+    def extend(self, item):
+        """Extend an item to the list"""
+        super().extend(item)
+        print("Extended the list with [{}] items".format(len(item)))
+        
+    def remove(self, item):
+        """Remove an item from the list"""
+        print("Removed [{}] from the list".format(item))
+        super().remove(item)
+
+    def pop(self, index=-1):
+        """Pop an item from the list"""
+        item = super().pop(index)
+        print("Popped [{}] from the list".format(item))
