ae2f2a067a8f0cdc00c453a155b984f358c8d9e0
Thomas3300000 <8844@holbertonstudents.com>
2024-05-27 09:43:59 +0200

    Fix docstyle

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""Define function read_file"""
 
 
 def read_file(filename=""):
-    """Define function that reads a text file and prints it to stdout"""
+    """Function that reads a text file and prints it to stdout"""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
