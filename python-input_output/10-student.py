3839871c0d2c88f3f9619cb61629e41ea8678b3d
Thomas3300000 <8844@holbertonstudents.com>
2024-05-28 13:32:07 +0200

    Only attribute names

---
+++ python-input_output/10-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Define a class Student."""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student
+
+        Args:
+            first_name (str): First name
+            last_name (str): Last name
+            age (int): Age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return a dictionary representation of a Student with specified attrs
+
+        Args:
+            attrs (list): List of attributes
+        
+        Returns:
+            dict: Dictionary representation of a Student
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_dict = {}
+            dictionary = self.__dict__
+            
+            for cpt in attrs:
+                if cpt in dictionary:
+                    new_dict[cpt] = dictionary[cpt]
+            return new_dict
