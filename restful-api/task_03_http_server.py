77e806dd276cd81233da8b0c5dd963530e02fb7d
Thomas3300000 <8844@holbertonstudents.com>
2024-06-02 22:04:00 +0200

    HTTP server

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+
+
+class classhttp(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API")
+        elif self.path == "/data":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(data).encode('utf-8'))
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"OK")
+        elif self.path == "/info":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b'{"version": "1.0", "description": "A simple API built with http.server"}')
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"404 Not Found")
+
+
+httpd = HTTPServer(("", 8000), classhttp)
+httpd.serve_forever()
