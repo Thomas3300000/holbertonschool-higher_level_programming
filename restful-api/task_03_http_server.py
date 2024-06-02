475680236721cfe129c5221c4d75a89cd6fedd6a
Thomas3300000 <8844@holbertonstudents.com>
2024-06-02 22:17:29 +0200

    Fix message path / HTTP server

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -9,7 +9,7 @@ class subclasshttp(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(b"Hello, this is a simple API")
+            self.wfile.write(b"Hello, this is a simple API!")
         elif self.path == "/data":
             self.send_response(200)
             self.send_header("Content-type", "application/json")
