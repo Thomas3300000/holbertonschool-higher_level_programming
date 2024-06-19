36edb9662381783ef890294211baff3394e1942e
Thomas3300000 <8844@holbertonstudents.com>
2024-06-19 11:20:28 +0200

    Fix '

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Script create user_0d_1
 -- 1-create_user.sql
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY user_0d_1_pwd;
-GRANTS ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY user_0d_1_pwd;
+GRANTS ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
