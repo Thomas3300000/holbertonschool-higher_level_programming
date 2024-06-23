e42ebc837fd09e4c63f711185abe0f0038e211e9
Thomas3300000 <8844@holbertonstudents.com>
2024-06-24 01:32:51 +0200

    Add files

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -3,6 +3,7 @@
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -12,3 +13,5 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
+    
+    cities = relationship("City", back_populates="state")
