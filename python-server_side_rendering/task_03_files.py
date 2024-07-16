f90d3bfb9ad03c28351e6ca9c18e8db312e6798b
Thomas3300000 <8844@holbertonstudents.com>
2024-07-17 00:03:49 +0200

    json csv an template display

---
+++ python-server_side_rendering/task_03_files.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template, request
+import json
+import csv
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/items')
+def items():
+    with open ('items.json') as file:
+        data = json.load(file)
+        list = data.get('items', [])
+        return render_template('items.html', items=list)
+    
+def read_json():
+    with open ('products.json') as f:
+        products = json.load(f)
+    return products
+
+def read_csv():
+    products = []
+    with open ('products.csv', newline='') as f:
+        list = csv.DictReader(f)
+        for row in list:
+            products.append({
+                "id": int(row['id']),
+                "name": row['name'],
+                "category": row['category'],
+                "price": float(row['price'])
+            })
+    return products
+
+@app.route('/products')
+def products_display():
+    source = request.args.get('source')
+    products_id = request.args.get('id', type=int)
+    products = []
+    
+    if source == 'json':
+        products = read_json()
+    elif source == 'csv':
+        products = read_csv()
+    else:
+        return render_template('products_display.html', error='Wrong source')
+        
+    if products_id:
+        products = [product for product in products if product['id'] == products_id]
+        if not products:
+            return render_template('products_display.html', error='Product not found')
+
+    return render_template('products_display.html', products=products)
+        
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
